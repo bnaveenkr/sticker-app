6e8c74db4b5ca5b05b88acf94c81e550a78c3e13
Naveen Kumar <bolla.naveenkumar@gmail.com>
2016-02-25 08:51:22 +0530

    Sort of working sticker upload

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
  /* Javascript */
 "use strict;"
 
-var stickerApp = angular.module('photoSticker', ["ui.router"]);
+var stickerApp = angular.module('photoSticker', ["ui.router", "ui.bootstrap"]);
 
 stickerApp.config(function($stateProvider, $urlRouterProvider) {
 $urlRouterProvider.otherwise("/");
@@ -45,3 +45,58 @@ stickerApp.controller('PhotoCtrl', ['$scope', function($scope){
 
 }])
 
+
+stickerApp.controller('StickerCtrl', ['$scope', '$uibModal', function($scope, $uibModal){
+    
+    $scope.stickers = [];    
+
+    $scope.open = function() {
+    var modalInstance = $uibModal.open({
+        templateUrl: 'stickerupload.html',
+        controller : 'StickerUploadCtrl'
+    })
+
+    modalInstance.result.then(function (sticker) {
+      $scope.stickers.push(sticker);
+    }, function () {
+        // do nothing for now.
+    });
+    };
+
+}]);
+
+stickerApp.controller("StickerUploadCtrl", ['$scope', '$uibModalInstance', function($scope, $uibModalInstance){
+    
+    $scope.sticker = {
+        'image' : '',
+        'title' : 'Some sticker'
+    }
+
+
+    $scope.imageUpload = function(event){
+         var files = event.target.files; //FileList object
+         
+         for (var i = 0; i < files.length; i++) {
+                 var file = files[i];
+                 var reader = new FileReader();
+                 reader.onload = $scope.imageIsLoaded; 
+                 reader.readAsDataURL(file);
+         }
+    }
+
+    $scope.imageIsLoaded = function(e){
+        $scope.$apply(function() {
+            $scope.sticker.image = e.target.result; 
+            console.log(e.target.result)
+        });
+    }
+
+$scope.ok = function () {
+    $uibModalInstance.close($scope.sticker);
+  };
+
+$scope.cancel = function(){
+    $uibModalInstance.dismiss('cancel');
+}
+
+}]);
